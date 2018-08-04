17faa937323a4d04ae57defa8c94cbeaaf8b1160
manfered <35757736+manfered@users.noreply.github.com>
2018-08-04 13:43:16 -0600

    Add project files.

---
+++ HMAC/HMAC.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HMAC
+{
+    public class HMAC
+    {
+        private const int KeySize = 32;
+
+        public static byte[] GenerateKey()
+        {
+            using(var randomNumberGenerator = new RNGCryptoServiceProvider())
+            {
+                var randomNumber = new byte[KeySize];
+                randomNumberGenerator.GetBytes(randomNumber);
+
+                return randomNumber;
+            }
+        }
+
+        public static byte[] ComputeHmacSha256(byte[] toBeHashed, byte[] key)
+        {
+            using(var hmac = new HMACSHA256(key))
+            {
+                return hmac.ComputeHash(toBeHashed);
+            }
+        }
+
+        public static byte[] ComputeHmacSha1(byte[] toBeHashed, byte[] key)
+        {
+            using(var hmac = new HMACSHA1(key))
+            {
+                return hmac.ComputeHash(toBeHashed);
+            }
+        }
+
+        public static byte[] ComputeHmacSha512(byte[] toBeHashed, byte[] key)
+        {
+            using(var hmac = new HMACSHA512(key))
+            {
+                return hmac.ComputeHash(toBeHashed);
+            }
+        }
+
+        public static byte[] ComputeHmacmd5(byte[] toBeHashed, byte[] key)
+        {
+            using(var hmac = new HMACMD5(key))
+            {
+                return hmac.ComputeHash(toBeHashed);
+            }
+        }
+    }
+}
---
+++ HMAC/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HMAC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string firstMessage = "First Message";
+            const string secondMessage = "Second Message";
+
+            Console.WriteLine("HMAC");
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine($"First Message : {firstMessage}");
+            Console.WriteLine($"Second Message : {secondMessage}");
+            Console.WriteLine();
+
+            var key = HMAC.GenerateKey();
+
+            var hmacMd5Message = HMAC.ComputeHmacmd5(Encoding.UTF8.GetBytes(firstMessage), key);
+            var hmacMd5Message2 = HMAC.ComputeHmacmd5(Encoding.UTF8.GetBytes(secondMessage), key);
+
+            var hmacSha1Message = HMAC.ComputeHmacSha1(Encoding.UTF8.GetBytes(firstMessage), key);
+            var hmacSha1Message2 = HMAC.ComputeHmacSha1(Encoding.UTF8.GetBytes(secondMessage), key);
+
+            var hmacSha256Message = HMAC.ComputeHmacSha256(Encoding.UTF8.GetBytes(firstMessage), key);
+            var hmacSha256Message2 = HMAC.ComputeHmacSha256(Encoding.UTF8.GetBytes(secondMessage), key);
+
+            var hmacSha512Message = HMAC.ComputeHmacSha512(Encoding.UTF8.GetBytes(firstMessage), key);
+            var hmacSha512Message2 = HMAC.ComputeHmacSha512(Encoding.UTF8.GetBytes(secondMessage), key);
+
+            Console.WriteLine();
+            Console.WriteLine("MD5 HMAC");
+            Console.WriteLine();
+            Console.WriteLine($"Fist Message : {Convert.ToBase64String(hmacMd5Message)}");
+            Console.WriteLine($"Second Message : {Convert.ToBase64String(hmacMd5Message2)}");
+
+            Console.WriteLine();
+            Console.WriteLine("SHA1 HMAC");
+            Console.WriteLine();
+            Console.WriteLine($"Fist Message : {Convert.ToBase64String(hmacSha1Message)}");
+            Console.WriteLine($"Second Message : {Convert.ToBase64String(hmacSha1Message2)}");
+
+            Console.WriteLine();
+            Console.WriteLine("SHA256 HMAC");
+            Console.WriteLine();
+            Console.WriteLine($"Fist Message : {Convert.ToBase64String(hmacSha256Message)}");
+            Console.WriteLine($"Second Message : {Convert.ToBase64String(hmacSha256Message2)}");
+
+            Console.WriteLine();
+            Console.WriteLine("SHA512 HMAC");
+            Console.WriteLine();
+            Console.WriteLine($"Fist Message : {Convert.ToBase64String(hmacSha512Message)}");
+            Console.WriteLine($"Second Message : {Convert.ToBase64String(hmacSha512Message2)}");
+
+        }
+    }
+}
